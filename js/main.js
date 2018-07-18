d9e5851f2a890f2e2a0e1511076eda38ef5a96fa
jaym97 <34835538+jaym97@users.noreply.github.com>
2018-07-18 23:42:59 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * @description Event listener to check for a click on either button 2 or 3
+ *
+ */
+
+$('#btn2, #btn3').click(function() {
+    //Alerts the user and displays contained message
+    alert('Sorry. This work has not been completed yet.');
+});
